f08ad1a7e2eff20a4b3211ecac636806de3b8465
Aman <sd95.aman@gmail.com>
2017-06-05 21:48:00 +0530

    Update Session.php

--- core/Session.php
+++ core/Session.php
@@ -1,39 +1,5 @@
 <?php
 
-// class Session {
-
-// 	public function __construct() {
-// 		session_start();
-// 	}
-
-// 	public function set_session($sid, $session_value) {
-// 		$_SESSION[$sid] = $session_value;
-// 		return true;
-// 	}
-
-// 	public function get_session($sid) {
-// 		return $_SESSION[$sid];
-// 	}
-
-// 	public function isSessionExist($sid) {
-// 		if(isset($_SESSION[$sid]) && !empty($_SESSION[$sid]))
-// 			return true;
-// 		return false;
-// 	}
-
-// 	public function sessionKill($sid) {
-// 		$_SESSION[$sid] = '';
-// 		unset($_SESSION[$sid]);
-// 	}
-
-// 	public function sessionKillAll() {
-// 		$_SESSION = array();
-// 		session_destroy();
-// 	}
-
-// }
-
-
 class Session
 {
 	
