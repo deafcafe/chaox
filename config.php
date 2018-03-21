755e23da7a3812abafa07c78bcc54bafa457ddc2
Amandeep Singh <sd95.aman@gmail.com>
2018-03-21 20:42:50 +0530

    Comments

--- config.php
+++ config.php
@@ -1,5 +1,8 @@
 <?php
-
+/*
+* This file will contain all the configurations needed to bootstrap the Application.
+* This file will return an Associative array containing configurations.
+*/
 return [
 	'database' => [
 		'ddsn' => 'mysql:host=127.0.0.1',
