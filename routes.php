6302420d2527350e840f5e744a26a1f1375a0d91
Amandeep Singh <sd95.aman@gmail.com>
2018-03-21 20:29:47 +0530

    Comments

--- routes.php
+++ routes.php
@@ -1,5 +1,8 @@
 <?php
 
+/*
+* GET Request Routes.
+*/
 $router->get('chatox', 'controllers/index.php');
 $router->get('chatox/views', 'controllers/index.php');
 $router->get('chatox/views/partials', 'controllers/index.php');
@@ -9,15 +12,15 @@ $router->get('chatox/chat', 'controllers/chat.php');
 $router->get('chatox/chat/show', 'controllers/poll.php');
 $router->get('chatox/logout', 'controllers/logout.php');
 $router->get('chatox/chat/meta', 'controllers/meta.php');
+$router->get('chatox/showimage', 'controllers/show_image.php');
 
+/*
+* POST Request Routes.
+*/
 $router->post('chatox/create', 'controllers/create_group.php');
 $router->post('chatox/join', 'controllers/join_group.php');
 $router->post('chatox/chat', 'controllers/chat.php');
 $router->post('chatox/set', 'controllers/set.php');
-
-$router->get('chatox/showimage', 'controllers/show_image.php');
-
-
 $router->post('chatox/showpeople', 'controllers/showpeople.php');
 
 
